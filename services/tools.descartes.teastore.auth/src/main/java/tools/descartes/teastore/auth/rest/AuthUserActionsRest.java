7dda969c13f7f0ef8c63b2a0bc8e22abf50339df
Martin Armbruster <armbrustermartin@googlemail.com>
2022-03-13 22:15:40 +0100

    Added the instrumented code.

--- services/tools.descartes.teastore.auth/src/main/java/tools/descartes/teastore/auth/rest/AuthUserActionsRest.java
+++ services/tools.descartes.teastore.auth/src/main/java/tools/descartes/teastore/auth/rest/AuthUserActionsRest.java
@@ -1,29 +1,13 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package tools.descartes.teastore.auth.rest;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-
 import tools.descartes.teastore.auth.security.BCryptProvider;
 import tools.descartes.teastore.auth.security.RandomSessionIdGenerator;
 import tools.descartes.teastore.auth.security.ShaSecurityProvider;
@@ -36,151 +20,146 @@ import tools.descartes.teastore.registryclient.loadbalancers.LoadBalancerTimeout
 import tools.descartes.teastore.registryclient.rest.LoadBalancedCRUDOperations;
 import tools.descartes.teastore.registryclient.util.NotFoundException;
 import tools.descartes.teastore.registryclient.util.TimeoutException;
-
-/**
- * Rest endpoint for the store user actions.
- * 
- * @author Simon
- */
 @Path("useractions")
-@Produces({ "application/json" })
-@Consumes({ "application/json" })
+@Produces({"application/json"})
+@Consumes({"application/json"})
 public class AuthUserActionsRest {
+@POST
+@Path("placeorder")
+public  Response placeOrder(SessionBlob blob, @QueryParam("totalPriceInCents")
+long totalPriceInCents, @QueryParam("addressName")
+String addressName, @QueryParam("address1")
+String address1, @QueryParam("address2")
+String address2, @QueryParam("creditCardCompany")
+String creditCardCompany, @QueryParam("creditCardNumber")
+String creditCardNumber, @QueryParam("creditCardExpiryDate")
+String creditCardExpiryDate) {
+cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController threadMonitoringController = cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController.getInstance();
+cipm.consistency.bridge.monitoring.controller.ServiceParameters monitoringServiceParameters = new  cipm.consistency.bridge.monitoring.controller.ServiceParameters();
+monitoringServiceParameters.addValue("blob", blob);
+monitoringServiceParameters.addValue("totalPriceInCents", totalPriceInCents);
+monitoringServiceParameters.addValue("addressName", addressName);
+monitoringServiceParameters.addValue("address1", address1);
+monitoringServiceParameters.addValue("address2", address2);
+monitoringServiceParameters.addValue("creditCardCompany", creditCardCompany);
+monitoringServiceParameters.addValue("creditCardNumber", creditCardNumber);
+monitoringServiceParameters.addValue("creditCardExpiryDate", creditCardExpiryDate);
+threadMonitoringController.enterService("_MtBc4ApFEeyde98rBgRSjw", this, monitoringServiceParameters);
+try {
+if (new  ShaSecurityProvider().validate(blob) == null || blob.getOrderItems().isEmpty())
+{
+return Response.status(Response.Status.NOT_FOUND).build();
+}
+blob.getOrder().setUserId(blob.getUID());
+blob.getOrder().setTotalPriceInCents(totalPriceInCents);
+blob.getOrder().setAddressName(addressName);
+blob.getOrder().setAddress1(address1);
+blob.getOrder().setAddress2(address2);
+blob.getOrder().setCreditCardCompany(creditCardCompany);
+blob.getOrder().setCreditCardExpiryDate(creditCardExpiryDate);
+blob.getOrder().setCreditCardNumber(creditCardNumber);
+blob.getOrder().setTime(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+long orderId;
+try {
+orderId = LoadBalancedCRUDOperations.sendEntityForCreation(Service.PERSISTENCE, "orders", Order.class, blob.getOrder());
+}
+catch(LoadBalancerTimeoutException e){
+return Response.status(408).build();
+}
+catch(NotFoundException e){
+return Response.status(404).build();
+}
+for (OrderItem item : blob.getOrderItems())
+{
+try {
+item.setOrderId(orderId);
+LoadBalancedCRUDOperations.sendEntityForCreation(Service.PERSISTENCE, "orderitems", OrderItem.class, item);
+}
+catch(TimeoutException e){
+return Response.status(408).build();
+}
+catch(NotFoundException e){
+return Response.status(404).build();
+}
+}
+blob.setOrder(new  Order());
+blob.getOrderItems().clear();
+blob = new  ShaSecurityProvider().secure(blob);
+return Response.status(Response.Status.OK).entity(blob).build();
+}
+finally {
+threadMonitoringController.exitService("_MtBc4ApFEeyde98rBgRSjw");
+}
+}
 
-  /**
-   * Persists order in database.
-   * 
-   * @param blob
-   *          SessionBlob
-   * @param totalPriceInCents
-   *          totalPrice
-   * @param addressName
-   *          address
-   * @param address1
-   *          address
-   * @param address2
-   *          address
-   * @param creditCardCompany
-   *          creditcard
-   * @param creditCardNumber
-   *          creditcard
-   * @param creditCardExpiryDate
-   *          creditcard
-   * @return Response containing SessionBlob
-   */
-  @POST
-  @Path("placeorder")
-  public Response placeOrder(SessionBlob blob,
-      @QueryParam("totalPriceInCents") long totalPriceInCents,
-      @QueryParam("addressName") String addressName, @QueryParam("address1") String address1,
-      @QueryParam("address2") String address2,
-      @QueryParam("creditCardCompany") String creditCardCompany,
-      @QueryParam("creditCardNumber") String creditCardNumber,
-      @QueryParam("creditCardExpiryDate") String creditCardExpiryDate) {
-    if (new ShaSecurityProvider().validate(blob) == null || blob.getOrderItems().isEmpty()) {
-      return Response.status(Response.Status.NOT_FOUND).build();
-    }
+@POST
+@Path("login")
+public  Response login(SessionBlob blob, @QueryParam("name")
+String name, @QueryParam("password")
+String password) {
+cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController threadMonitoringController = cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController.getInstance();
+cipm.consistency.bridge.monitoring.controller.ServiceParameters monitoringServiceParameters = new  cipm.consistency.bridge.monitoring.controller.ServiceParameters();
+monitoringServiceParameters.addValue("blob", blob);
+monitoringServiceParameters.addValue("name", name);
+monitoringServiceParameters.addValue("password", password);
+threadMonitoringController.enterService("_Mty48ApFEeyde98rBgRSjw", this, monitoringServiceParameters);
+try {
+User user;
+try {
+user = LoadBalancedCRUDOperations.getEntityWithProperties(Service.PERSISTENCE, "users", User.class, "name", name);
+}
+catch(TimeoutException e){
+return Response.status(408).build();
+}
+catch(NotFoundException e){
+return Response.status(Response.Status.OK).entity(blob).build();
+}
+if (user != null && BCryptProvider.checkPassword(password, user.getPassword()))
+{
+blob.setUID(user.getId());
+blob.setSID(new  RandomSessionIdGenerator().getSessionId());
+blob = new  ShaSecurityProvider().secure(blob);
+return Response.status(Response.Status.OK).entity(blob).build();
+}
+return Response.status(Response.Status.OK).entity(blob).build();
+}
+finally {
+threadMonitoringController.exitService("_Mty48ApFEeyde98rBgRSjw");
+}
+}
 
-    blob.getOrder().setUserId(blob.getUID());
-    blob.getOrder().setTotalPriceInCents(totalPriceInCents);
-    blob.getOrder().setAddressName(addressName);
-    blob.getOrder().setAddress1(address1);
-    blob.getOrder().setAddress2(address2);
-    blob.getOrder().setCreditCardCompany(creditCardCompany);
-    blob.getOrder().setCreditCardExpiryDate(creditCardExpiryDate);
-    blob.getOrder().setCreditCardNumber(creditCardNumber);
-    blob.getOrder().setTime(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+@POST
+@Path("logout")
+public  Response logout(SessionBlob blob) {
+cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController threadMonitoringController = cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController.getInstance();
+cipm.consistency.bridge.monitoring.controller.ServiceParameters monitoringServiceParameters = new  cipm.consistency.bridge.monitoring.controller.ServiceParameters();
+monitoringServiceParameters.addValue("blob", blob);
+threadMonitoringController.enterService("_MuWSkApFEeyde98rBgRSjw", this, monitoringServiceParameters);
+try {
+blob.setUID(null);
+blob.setSID(null);
+blob.setOrder(new  Order());
+blob.getOrderItems().clear();
+return Response.status(Response.Status.OK).entity(blob).build();
+}
+finally {
+threadMonitoringController.exitService("_MuWSkApFEeyde98rBgRSjw");
+}
+}
 
-    long orderId;
-    try {
-      orderId = LoadBalancedCRUDOperations.sendEntityForCreation(Service.PERSISTENCE, "orders",
-          Order.class, blob.getOrder());
-    } catch (LoadBalancerTimeoutException e) {
-      return Response.status(408).build();
-    } catch (NotFoundException e) {
-      return Response.status(404).build();
-    }
-    for (OrderItem item : blob.getOrderItems()) {
-      try {
-        item.setOrderId(orderId);
-        LoadBalancedCRUDOperations.sendEntityForCreation(Service.PERSISTENCE, "orderitems",
-            OrderItem.class, item);
-      } catch (TimeoutException e) {
-        return Response.status(408).build();
-      } catch (NotFoundException e) {
-        return Response.status(404).build();
-      }
-    }
-    blob.setOrder(new Order());
-    blob.getOrderItems().clear();
-    blob = new ShaSecurityProvider().secure(blob);
-    return Response.status(Response.Status.OK).entity(blob).build();
-  }
-
-  /**
-   * User login.
-   * 
-   * @param blob
-   *          SessionBlob
-   * @param name
-   *          Username
-   * @param password
-   *          password
-   * @return Response with SessionBlob containing login information.
-   */
-  @POST
-  @Path("login")
-  public Response login(SessionBlob blob, @QueryParam("name") String name,
-      @QueryParam("password") String password) {
-    User user;
-    try {
-      user = LoadBalancedCRUDOperations.getEntityWithProperties(Service.PERSISTENCE, "users",
-          User.class, "name", name);
-    } catch (TimeoutException e) {
-      return Response.status(408).build();
-    } catch (NotFoundException e) {
-      return Response.status(Response.Status.OK).entity(blob).build();
-    }
-
-    if (user != null && BCryptProvider.checkPassword(password, user.getPassword())
-    ) {
-      blob.setUID(user.getId());
-      blob.setSID(new RandomSessionIdGenerator().getSessionId());
-      blob = new ShaSecurityProvider().secure(blob);
-      return Response.status(Response.Status.OK).entity(blob).build();
-    }
-    return Response.status(Response.Status.OK).entity(blob).build();
-  }
-
-  /**
-   * User logout.
-   * 
-   * @param blob
-   *          SessionBlob
-   * @return Response with SessionBlob
-   */
-  @POST
-  @Path("logout")
-  public Response logout(SessionBlob blob) {
-    blob.setUID(null);
-    blob.setSID(null);
-    blob.setOrder(new Order());
-    blob.getOrderItems().clear();
-    return Response.status(Response.Status.OK).entity(blob).build();
-  }
-
-  /**
-   * Checks if user is logged in.
-   * 
-   * @param blob
-   *          Sessionblob
-   * @return Response with true if logged in
-   */
-  @POST
-  @Path("isloggedin")
-  public Response isLoggedIn(SessionBlob blob) {
-    return Response.status(Response.Status.OK).entity(new ShaSecurityProvider().validate(blob))
-        .build();
-  }
+@POST
+@Path("isloggedin")
+public  Response isLoggedIn(SessionBlob blob) {
+cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController threadMonitoringController = cipm.consistency.bridge.monitoring.controller.ThreadMonitoringController.getInstance();
+cipm.consistency.bridge.monitoring.controller.ServiceParameters monitoringServiceParameters = new  cipm.consistency.bridge.monitoring.controller.ServiceParameters();
+monitoringServiceParameters.addValue("blob", blob);
+threadMonitoringController.enterService("_MujG4ApFEeyde98rBgRSjw", this, monitoringServiceParameters);
+try {
+return Response.status(Response.Status.OK).entity(new  ShaSecurityProvider().validate(blob)).build();
+}
+finally {
+threadMonitoringController.exitService("_MujG4ApFEeyde98rBgRSjw");
+}
+}
 
 }
--- services/tools.descartes.teastore.recommender/src/main/java/tools/descartes/teastore/recommender/servlet/TrainingSynchronizer.java
+++ services/tools.descartes.teastore.recommender/src/main/java/tools/descartes/teastore/recommender/servlet/TrainingSynchronizer.java
@@ -1,16 +1,3 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package tools.descartes.teastore.recommender.servlet;
 
 import java.time.Instant;
@@ -23,13 +10,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import tools.descartes.teastore.recommender.algorithm.RecommenderSelector;
 import tools.descartes.teastore.registryclient.Service;
 import tools.descartes.teastore.registryclient.loadbalancers.LoadBalancerTimeoutException;
@@ -38,242 +22,192 @@ import tools.descartes.teastore.registryclient.rest.LoadBalancedCRUDOperations;
 import tools.descartes.teastore.registryclient.util.NotFoundException;
 import tools.descartes.teastore.entities.Order;
 import tools.descartes.teastore.entities.OrderItem;
-
-/**
- * This class organizes the communication with the other services and
- * synchronizes on startup and training.
- * 
- * @author Johannes Grohmann
- *
- */
 public final class TrainingSynchronizer {
+public static final long DEFAULT_MAX_TIME_VALUE = Long.MIN_VALUE;
 
-	/**
-	 * This value signals that the maximum training time is not known.
-	 */
-	public static final long DEFAULT_MAX_TIME_VALUE = Long.MIN_VALUE;
+private static final int PERSISTENCE_CREATION_MAX_WAIT_TIME = 120000;
 
-	// Longest wait period before querying the persistence again if it is finished
-	// creating entries
-	private static final int PERSISTENCE_CREATION_MAX_WAIT_TIME = 120000;
-	// Wait time in ms before checking again for an existing persistence service
-	private static final List<Integer> PERSISTENCE_CREATION_WAIT_TIME = Arrays.asList(1000, 2000, 5000, 10000, 30000,
-			60000);
+private static final List<Integer> PERSISTENCE_CREATION_WAIT_TIME = Arrays.asList(1000, 2000, 5000, 10000, 30000, 60000);
 
-	private static TrainingSynchronizer instance;
+private static TrainingSynchronizer instance;
 
-	private boolean isReady = false;
+private boolean isReady = false;
 
-	/**
-	 * @return the isReady
-	 */
-	public boolean isReady() {
-		return isReady;
-	}
+public  boolean isReady() {
+return isReady;
+}
 
-	/**
-	 * @param isReady
-	 *            the isReady to set
-	 */
-	public void setReady(boolean isReady) {
-		this.isReady = isReady;
-	}
+public  void setReady(boolean isReady) {
+this.isReady = isReady;
+}
 
-	private TrainingSynchronizer() {
+private  TrainingSynchronizer(){
+}
+public static  TrainingSynchronizer getInstance() {
+if (instance == null)
+{
+instance = new  TrainingSynchronizer();
+}
+return instance;
+}
 
-	}
+private static final Logger LOG = LoggerFactory.getLogger(TrainingSynchronizer.class);
 
-	/**
-	 * Returns the instance for this singleton.
-	 * 
-	 * @return An instance of {@link TrainingSynchronizer}
-	 */
-	public static TrainingSynchronizer getInstance() {
-		if (instance == null) {
-			instance = new TrainingSynchronizer();
-		}
-		return instance;
+private long maxTime = DEFAULT_MAX_TIME_VALUE;
 
-	}
+public  long getMaxTime() {
+return maxTime;
+}
 
-	private static final Logger LOG = LoggerFactory.getLogger(TrainingSynchronizer.class);
+public  void setMaxTime(String maxTime) {
+setMaxTime(toMillis(maxTime));
+}
 
-	/**
-	 * The maximum considered time in milliseconds. DEFAULT_MAX_TIME_VALUE signals
-	 * no entry, e.g. all orders are used for training.
-	 */
-	private long maxTime = DEFAULT_MAX_TIME_VALUE;
+public  void setMaxTime(long maxTime) {
+this.maxTime = maxTime;
+}
 
-	/**
-	 * @return the maxTime
-	 */
-	public long getMaxTime() {
-		return maxTime;
-	}
+private  void waitForPersistence() {
+Iterator<Integer> waitTimes = PERSISTENCE_CREATION_WAIT_TIME.iterator();
+while (true)
+{
+Response result = null;
+try {
+result = ServiceLoadBalancer.loadBalanceRESTOperation(Service.PERSISTENCE, "generatedb", String.class, client -> client.getService().path(client.getApplicationURI()).path(client.getEndpointURI()).path("finished").request().get());
+if (result != null && Boolean.parseBoolean(result.readEntity(String.class)))
+{
+break;
+}
+}
+catch(NotFoundException | LoadBalancerTimeoutException e){
+}
+finally {
+if (result != null)
+{
+result.close();
+}
+}
+try {
+int nextWaitTime;
+if (waitTimes.hasNext())
+{
+nextWaitTime = waitTimes.next();
+}
+else
+{
+nextWaitTime = PERSISTENCE_CREATION_MAX_WAIT_TIME;
+}
+LOG.info("Persistence not reachable. Waiting for {}ms.", nextWaitTime);
+Thread.sleep(nextWaitTime);
+}
+catch(InterruptedException interrupted){
+LOG.warn("Thread interrupted while waiting for persistence to be available.", interrupted);
+}
+}
+}
 
-	/**
-	 * @param maxTime
-	 *            the maxTime to set
-	 */
-	public void setMaxTime(String maxTime) {
-		setMaxTime(toMillis(maxTime));
-	}
+public  long retrieveDataAndRetrain() {
+setReady(false);
+LOG.trace("Retrieving data objects from database...");
+waitForPersistence();
+List<OrderItem> items = null;
+List<Order> orders = null;
+try {
+items = LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE, "orderitems", OrderItem.class,  - 1,  - 1);
+long noItems = items.size();
+LOG.trace("Retrieved " + noItems + " orderItems, starting retrieving of orders now.");
+}
+catch(Exception e){
+setReady(true);
+LOG.error("Database retrieving failed.");
+return  - 1;
+}
+try {
+orders = LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE, "orders", Order.class,  - 1,  - 1);
+long noOrders = orders.size();
+LOG.trace("Retrieved " + noOrders + " orders, starting training now.");
+}
+catch(Exception e){
+setReady(true);
+LOG.error("Database retrieving failed.");
+return  - 1;
+}
+filterLists(items, orders);
+RecommenderSelector.getInstance().train(items, orders);
+LOG.trace("Finished training, ready for recommendation.");
+setReady(true);
+return items.size() + orders.size();
+}
 
-	/**
-	 * @param maxTime
-	 *            the maxTime to set
-	 */
-	public void setMaxTime(long maxTime) {
-		this.maxTime = maxTime;
-	}
+private  void filterLists(List<OrderItem> orderItems, List<Order> orders) {
+List<Response> maxTimeResponses = ServiceLoadBalancer.multicastRESTOperation(Service.RECOMMENDER, "train/timestamp", Response.class, client -> client.getService().path(client.getApplicationURI()).path(client.getEndpointURI()).request(MediaType.TEXT_PLAIN).accept(MediaType.TEXT_PLAIN).get());
+for (Response response : maxTimeResponses)
+{
+if (response == null)
+{
+LOG.warn("One service response was null and is therefore not available for time-check.");
+}
+else
+if (response.getStatus() == Response.Status.OK.getStatusCode())
+{
+long milliTS = response.readEntity(Long.class);
+if (maxTime != TrainingSynchronizer.DEFAULT_MAX_TIME_VALUE && maxTime != milliTS)
+{
+LOG.warn("Services disagree about timestamp: " + maxTime + " vs " + milliTS + ". Therfore using the minimum.");
+}
+maxTime = Math.min(maxTime, milliTS);
+}
+else
+{
+response.bufferEntity();
+LOG.warn("Service " + response + "was not available for time-check.");
+}
+}
+if (maxTime == Long.MIN_VALUE)
+{
+for (Order or : orders)
+{
+maxTime = Math.max(maxTime, toMillis(or.getTime()));
+}
+}
+filterForMaxtimeStamp(orderItems, orders);
+}
 
-	private void waitForPersistence() {
-		// We have to wait for the database that all entries are created before
-		// generating images (which queries persistence). Yes we want to wait forever in
-		// case the persistence is
-		// not answering.
-		Iterator<Integer> waitTimes = PERSISTENCE_CREATION_WAIT_TIME.iterator();
-		while (true) {
-			Response result = null;
-			try {
-				result = ServiceLoadBalancer.loadBalanceRESTOperation(Service.PERSISTENCE, "generatedb", String.class,
-						client -> client.getService().path(client.getApplicationURI()).path(client.getEndpointURI())
-								.path("finished").request().get());
+private  void filterForMaxtimeStamp(List<OrderItem> orderItems, List<Order> orders) {
+List<Order> remove = new  ArrayList<>();
+for (Order or : orders)
+{
+if (toMillis(or.getTime()) > maxTime)
+{
+remove.add(or);
+}
+}
+orders.removeAll(remove);
+List<OrderItem> removeItems = new  ArrayList<>();
+for (OrderItem orderItem : orderItems)
+{
+boolean contained = false;
+for (Order or : orders)
+{
+if (or.getId() == orderItem.getOrderId())
+{
+contained = true;
+}
+}
+if (!contained)
+{
+removeItems.add(orderItem);
+}
+}
+orderItems.removeAll(removeItems);
+}
 
-				if (result != null && Boolean.parseBoolean(result.readEntity(String.class))) {
-					break;
-				}
-			} catch (NotFoundException | LoadBalancerTimeoutException e) {
-				// continue waiting as usual
-			} finally {
-				if (result != null) {
-					result.close();
-				}
-			}
-			try {
-				int nextWaitTime;
-				if (waitTimes.hasNext()) {
-					nextWaitTime = waitTimes.next();
-				} else {
-					nextWaitTime = PERSISTENCE_CREATION_MAX_WAIT_TIME;
-				}
-				LOG.info("Persistence not reachable. Waiting for {}ms.", nextWaitTime);
-				Thread.sleep(nextWaitTime);
-			} catch (InterruptedException interrupted) {
-				LOG.warn("Thread interrupted while waiting for persistence to be available.", interrupted);
-			}
-		}
-	}
-
-	/**
-	 * Connects via REST to the database and retrieves all {@link OrderItem}s and
-	 * all {@link Order}s. Then, it triggers the training of the recommender.
-	 * 
-	 * @return The number of elements retrieved from the database or -1 if the
-	 *         process failed.
-	 */
-	public long retrieveDataAndRetrain() {
-		setReady(false);
-		LOG.trace("Retrieving data objects from database...");
-
-		waitForPersistence();
-
-		List<OrderItem> items = null;
-		List<Order> orders = null;
-		// retrieve
-		try {
-			items = LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE, "orderitems", OrderItem.class, -1, -1);
-			long noItems = items.size();
-			LOG.trace("Retrieved " + noItems + " orderItems, starting retrieving of orders now.");
-		} catch (Exception e) {
-			// set ready anyway to avoid deadlocks
-			setReady(true);
-			LOG.error("Database retrieving failed.");
-			return -1;
-		}
-		try {
-			orders = LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE, "orders", Order.class, -1, -1);
-			long noOrders = orders.size();
-			LOG.trace("Retrieved " + noOrders + " orders, starting training now.");
-		} catch (Exception e) {
-			// set ready anyway to avoid deadlocks
-			setReady(true);
-			LOG.error("Database retrieving failed.");
-			return -1;
-		}
-		// filter lists
-		filterLists(items, orders);
-		// train instance
-		RecommenderSelector.getInstance().train(items, orders);
-		LOG.trace("Finished training, ready for recommendation.");
-		setReady(true);
-		return items.size() + orders.size();
-	}
-
-	private void filterLists(List<OrderItem> orderItems, List<Order> orders) {
-		// since we are not registered ourselves, we can multicast to all services
-		List<Response> maxTimeResponses = ServiceLoadBalancer.multicastRESTOperation(Service.RECOMMENDER,
-				"train/timestamp", Response.class,
-				client -> client.getService().path(client.getApplicationURI()).path(client.getEndpointURI())
-						.request(MediaType.TEXT_PLAIN).accept(MediaType.TEXT_PLAIN).get());
-		for (Response response : maxTimeResponses) {
-			if (response == null) {
-				LOG.warn("One service response was null and is therefore not available for time-check.");
-			} else if (response.getStatus() == Response.Status.OK.getStatusCode()) {
-				// only consider if status was fine
-				long milliTS = response.readEntity(Long.class);
-				if (maxTime != TrainingSynchronizer.DEFAULT_MAX_TIME_VALUE && maxTime != milliTS) {
-					LOG.warn("Services disagree about timestamp: " + maxTime + " vs " + milliTS
-							+ ". Therfore using the minimum.");
-				}
-				maxTime = Math.min(maxTime, milliTS);
-			} else {
-				// release connection by buffering entity
-				response.bufferEntity();
-				LOG.warn("Service " + response + "was not available for time-check.");
-			}
-		}
-		if (maxTime == Long.MIN_VALUE) {
-			// we are the only known service
-			// therefore we find max and set it
-			for (Order or : orders) {
-				maxTime = Math.max(maxTime, toMillis(or.getTime()));
-			}
-		}
-		filterForMaxtimeStamp(orderItems, orders);
-	}
-
-	private void filterForMaxtimeStamp(List<OrderItem> orderItems, List<Order> orders) {
-		// filter orderItems and orders and ignore newer entries.
-		List<Order> remove = new ArrayList<>();
-		for (Order or : orders) {
-			if (toMillis(or.getTime()) > maxTime) {
-				remove.add(or);
-			}
-		}
-		orders.removeAll(remove);
-
-		List<OrderItem> removeItems = new ArrayList<>();
-		for (OrderItem orderItem : orderItems) {
-			boolean contained = false;
-			for (Order or : orders) {
-				if (or.getId() == orderItem.getOrderId()) {
-					contained = true;
-				}
-			}
-			if (!contained) {
-				removeItems.add(orderItem);
-			}
-		}
-		orderItems.removeAll(removeItems);
-	}
-
-	private long toMillis(String date) {
-		TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_LOCAL_DATE_TIME.parse(date);
-		LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
-		ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
-		Instant instant = Instant.from(zonedDateTime);
-		return instant.toEpochMilli();
-	}
+private  long toMillis(String date) {
+TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_LOCAL_DATE_TIME.parse(date);
+LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
+ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
+Instant instant = Instant.from(zonedDateTime);
+return instant.toEpochMilli();
+}
 
 }
