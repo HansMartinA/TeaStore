0fd6eb65d07e1a68626a0679839b413c6a9bb46a
Martin Armbruster <armbrustermartin@googlemail.com>
2022-03-13 21:28:25 +0100

    Added the instrumented code.

--- utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/rest/HttpWrapper.java
+++ utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/rest/HttpWrapper.java
@@ -2,28 +2,13 @@ package tools.descartes.teastore.registryclient.rest;
 
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.core.MediaType;
-
 import javax.ws.rs.client.WebTarget;
-
-/**
- * Wrapper for http calls, acts as an extension point.
- * @author Simon
- *
- */
 public final class HttpWrapper {
+private  HttpWrapper(){
+}
+public static  Builder wrap(WebTarget target) {
+Builder builder = target.request(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+return builder;
+}
 
-  private HttpWrapper() {
-    
-  }
-  
-  /**
-   * Wrapper for http calls.
-   * @param target webtarget
-   * @return builder
-   */
-	public static Builder wrap(WebTarget target) {
-		Builder builder = target.request(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
-
-		return builder;
-	}
 }
